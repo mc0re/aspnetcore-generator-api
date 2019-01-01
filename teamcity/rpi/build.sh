01bbf12f2e39c2cfe713b9613531582cb2ea3cc0
mc0re <mnikitin@hotmail.com>
2019-01-01 21:00:43 +0100

    TeamCity on Raspberry.

--- teamcity/rpi/build.sh
+++ teamcity/rpi/build.sh
@@ -1,2 +1,6 @@
 docker build -t image-repo:55000/teamcity:armhf-latest common
 docker push image-repo:55000/teamcity:armhf-latest
+
+docker manifest create --insecure image-repo:55000/teamcity:latest image-repo:55000/teamcity:armhf-latest
+docker manifest annotate image-repo:55000/teamcity:latest image-repo:55000/teamcity:armhf-latest --os linux --arch arm
+docker manifest push image-repo:55000/teamcity:latest
