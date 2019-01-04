27dbd4bf6662132f0c159e254100b0fe8a2caa20
mc0re <mnikitin@hotmail.com>
2019-01-04 21:00:44 +0100

    Adding Raspberry agent to TeamCity.

--- teamcity/rpi/build.sh
+++ teamcity/rpi/build.sh
@@ -6,4 +6,27 @@ docker manifest annotate image-repo:55000/teamcity:latest image-repo:55000/teamc
 docker manifest push image-repo:55000/teamcity:latest
 
 docker build -t teamcity-server server
+
+# Docker-in-docker does not work (missing "RUN --mount" option),
+# so we build docker-compose locally.
+mkdir -p ~/compose
+pushd .
+cd ~/compose
+git clone https://github.com/docker/compose.git
+cd compose
+git checkout release
+docker build -t docker-compose:armhf -f Dockerfile.armhf .
+popd
+mkdir compose-dist
+docker run \
+    --rm --entrypoint="script/build/linux-entrypoint" \
+    -v $(pwd)/compose-dist:/code/dist \
+    -v ~/compose/.git:/code/.git \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    "docker-compose:armhf"
+rm -rf ~/compose
+docker image ls docker-compose -q | xargs -r docker image rm
+mv compose-dist/docker-compose-Linux-armv7l compose-dist/docker-compose
+
+# Now docker-compose is in a subdirectory "compose-dist*"
 docker build -t teamcity-agent agent
