21e571bdf3412e475b7ee28b79d64d9e5202d53c
mc0re <mnikitin@hotmail.com>
2019-01-04 22:31:43 +0100

    Adding Raspberry agent to TeamCity.

--- teamcity/rpi/build.sh
+++ teamcity/rpi/build.sh
@@ -9,7 +9,7 @@ docker build -t teamcity-server server
 
 # Docker-in-docker does not work (missing "RUN --mount" option),
 # so we build docker-compose locally.
-if [ ! -f compose-dist/docker-compose ]; then
+if [ ! -f agent/compose-dist/docker-compose ]; then
 mkdir -p ~/compose
 pushd .
 cd ~/compose
@@ -18,18 +18,18 @@ cd compose
 git checkout release
 docker build -t docker-compose:armhf -f Dockerfile.armhf .
 popd
-mkdir -p compose-dist
+mkdir -p agent/compose-dist
 docker run \
     --entrypoint="script/build/linux-entrypoint" \
-    -v $(pwd)/compose-dist:/code/dist \
+    -v $(pwd)/../agent/compose-dist:/code/dist \
     -v ~/compose/compose/.git:/code/.git \
     -v /var/run/docker.sock:/var/run/docker.sock \
     "docker-compose:armhf"
 rm -rf ~/compose
-mv compose-dist/docker-compose-Linux-armv7l compose-dist/docker-compose
-chmod 755 compose-dist/docker-compose
+mv agent/compose-dist/docker-compose-Linux-armv7l agent/compose-dist/docker-compose
+chmod 755 agent/compose-dist/docker-compose
 # docker image ls docker-compose -q | xargs -r docker image rm
 fi
 
-# Now docker-compose is in a subdirectory "compose-dist*"
+# Now docker-compose is in a subdirectory "agent/compose-dist"
 docker build -t teamcity-agent agent
