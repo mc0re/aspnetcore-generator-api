b4a810480832dc1ce1ebc645b5ca03d9c7b276d6
mc0re <mnikitin@hotmail.com>
2019-01-04 23:06:50 +0100

    Adding Raspberry agent to TeamCity.

--- teamcity/rpi/pi-agent-for-vm.sh
+++ teamcity/rpi/pi-agent-for-vm.sh
@@ -1,4 +1,3 @@
 docker run -d --name teamcity-agent -v /var/run/docker.sock:/var/run/docker.sock teamcity-agent
-echo Press Ctrl-C to stop the logs (not the agent)
+echo "Press Ctrl-C to stop the logs (not the agent)"
 docker logs teamcity-agent -f
-
