cd7caf9e55eeeb8c062e055c5debdc3824748e56
Mike <mnikitin@hotmail.com>
2019-01-06 12:42:04 +0000

    TeamCity agent on Pi.

--- teamcity/rpi/pi-agent-for-vm.sh
+++ teamcity/rpi/pi-agent-for-vm.sh
@@ -1,3 +1,3 @@
 docker run -d --name teamcity-agent -v /var/run/docker.sock:/var/run/docker.sock teamcity-agent
 echo "Press Ctrl-C to stop the logs (not the agent)"
-docker logs teamcity-agent -f
+docker logs -f teamcity-agent
