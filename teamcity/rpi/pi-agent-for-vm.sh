cfbe702894f4b16077d9deff99a16d119f0a8c87
Mike <mnikitin@hotmail.com>
2019-01-06 18:34:51 +0000

    Adjustment to agent script.

--- teamcity/rpi/pi-agent-for-vm.sh
+++ teamcity/rpi/pi-agent-for-vm.sh
@@ -1,3 +1,4 @@
-docker run -d --name teamcity-agent -v /var/run/docker.sock:/var/run/docker.sock teamcity-agent
+docker container prune -f
+docker run -d --name teamcity-agent -v /var/run/docker.sock:/var/run/docker.sock image-repo:55000/teamcity-agent
 echo "Press Ctrl-C to stop the logs (not the agent)"
 docker logs -f teamcity-agent
